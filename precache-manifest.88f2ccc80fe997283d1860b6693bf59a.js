3bac2c5cf3be54f630556cea0756366142b8601e
Sofyan Sitorus <sofyansitorus@gmail.com>
2019-08-12 21:27:59 +0700

    Updates

---
+++ precache-manifest.88f2ccc80fe997283d1860b6693bf59a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e1ae214db9665adf42270cf2a3b6f6",
+    "url": "/Bulk-GitHub-Issue-Creator/index.html"
+  },
+  {
+    "revision": "e3d3f77572441376ca52",
+    "url": "/Bulk-GitHub-Issue-Creator/static/css/2.18033dd5.chunk.css"
+  },
+  {
+    "revision": "e3d3f77572441376ca52",
+    "url": "/Bulk-GitHub-Issue-Creator/static/js/2.aa346518.chunk.js"
+  },
+  {
+    "revision": "12c9c8cbee81ab17cc6a",
+    "url": "/Bulk-GitHub-Issue-Creator/static/js/main.928e460a.chunk.js"
+  },
+  {
+    "revision": "9ec01d761f1f20d4e0bf",
+    "url": "/Bulk-GitHub-Issue-Creator/static/js/runtime~main.829d19e3.js"
+  }
+]);
